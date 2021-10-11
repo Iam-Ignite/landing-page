b4743238546c251f7361413082248b7b894d1737
Faith Godwin <82447117+Iam-Ignite@users.noreply.github.com>
2021-10-11 15:45:24 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './index.css';
+import Navbar from './components/sections/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+    </div>
+  );
+}
+
+export default App;
